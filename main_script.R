4e855179bfb55beec8e807d87801a21f8f14ad65
MLobo1997 <miguelobo1997@gmail.com>
2018-11-23 16:45:41 +0000

    split files and organized stuff

--- main_script.R
+++ main_script.R
@@ -1,45 +1,13 @@
-
-
+library(boot)
 #projects1 = read.csv("datasets/ks-projects-201612.csv",header=T,na.strings="?")
 projects = read.csv("datasets/ks-projects-201801.csv",header=T,na.strings="?") #Talvez seja melhor apenas analisarmos um dos datasets dado que ambos têm dimensões suficientemente grandes?
+names(projects)
+projects <- projects[,-c(7, 9, 10, 11, 13)] #são removidos os atributos que não faz sentido utilizarmos (não os que não sao úteis)
 
 #attach(projects)
 
 dim(projects)
 names(projects)
-
-#########Compreensão dos atributos#########
-
-length(unique(projects$ID)) #Não útil
-
-length(unique(projects$name)) #Não útil neste estado, talvez extraindo quantidades (e.g.: número de caracteres ou palavras) ou realizando nlp
-
-length(unique(projects$deadline)) #É necessário arranjar outra forma de interpretar as datas para além de atributo nominativo. Hipóteses:
-#-Ter vários atributos, um para cada componente relevante da data
-#-Ter um valor que é a diferença da deadline com a epoch
-
-
-length(unique(projects$category)) #demasiados valores únicos para ser utilizado como atributo nominativo, desnecessário a não ser que se faça algum tratamento. Talvez ver utilizar a frequencia da categoria?
-
-unique(projects$main_category) #Provavelmente muito útil
-
-unique(projects$currency) #Provavelmente útil
-
-summary(projects$goal) #É importante utilizar o usd_goal_real em vez deste, por ser uniforme e estar convertido para USD
-summary(projects$usd_goal_real) #Provavelmente muito útil
-
-
-length(unique(projects$launched)) #Tal como com "deadline" é necessário arranjar outra forma de tratar as datas
-
-unique(projects$state) #Este atributo está demasiado relacionado com o atributo objetivo para ser utilizado
-
-unique(projects$country) #Possivelmente útil
-
-summary(projects$backers) #Este também não seria um atributo objetivo?
-
-summary(projects$pledged) #Para não ter valores em moedas diferentes é necessário que utilizemos apenas o atributo usd.pledged ou usd.pledged.real
-summary(projects$usd.pledged) #Tendo em conta que a conversão do atributo goal só existe através da Fixer.io api, convém utilizarmos a mesma para o pledged.
-summary(projects$usd_pledged_real) #Classe, atributo objetivo
 
 ##########Tratamento de dados#########
 
@@ -58,15 +26,24 @@ rm("lm.fit0")
 rm("dataset0")
 detach(dataset0)
 
-dataset1 <- projects[,c(4,15,14)]  #Removeu-se também o atributo currency e country para reduzir a complexidade
-dataset1$launch.deadline.diff = difftime(as.Date(projects[,"deadline"]), as.Date(projects[,"launched"]))
+#########Agora a sério#########
 
-attach(dataset1)
+set.seed(125)
+indexes <- sample(1:dim(projects)[1], 5000)
+projects <- projects[indexes,]  #Removeu-se também o atributo currency e country para reduzir a complexidade
+projects$duration = difftime(as.Date(projects[indexes,"deadline"]), as.Date(projects[indexes,"launched"]))
+names(projects)
+projects <- projects[, c("main_category", "usd_goal_real", "duration", "usd_pledged_real")]
+summary(projects)
 
-plot(launch.deadline.diff, usd_pledged_real) #se se remover aquele outlier dos 1500 vê se talvez uma correlação quadrática
+attach(projects)
 
-lm.fit1 <- lm(usd_pledged_real ~ main_category + usd_goal_real + launch.deadline.diff)
+plot(duration, usd_pledged_real) #se se remover aquele outlier dos 1500 vê se talvez uma correlação quadrática
+plot(duration, log(usd_pledged_real)) #se se remover aquele outlier dos 1500 vê se talvez uma correlação quadrática
+
+lm.fit1 <- glm(usd_pledged_real~., data = projects)
 summary(lm.fit1) #modelo continuar a ser muito fraco
+cv.err <- cv.glm(projects, lm.fit1, K = 100)$delta[1]
+cv.err
 
-rm("dataset1")
-detach(dataset1)
+
